df672b7dfae5b7b083cb97a44b35d8c11b3ae56d
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-10-14 20:58:27 +0300

    add first programm

--- smth_1/Program.cs
+++ smth_1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Задача про номер дня недели
+// Console.WriteLine("Введите номер дня недели: ");
+// int number_for_day_of_the_week = Convert.ToInt32(Console.ReadLine());
+// if(number_for_day_of_the_week >=1 && number_for_day_of_the_week <=7)
+// {
+//     if(number_for_day_of_the_week == 1) Console.WriteLine("Понедельник");
+//     if(number_for_day_of_the_week == 2) Console.WriteLine("Вторник");
+//     if(number_for_day_of_the_week == 3) Console.WriteLine("Среда");
+//     if(number_for_day_of_the_week == 4) Console.WriteLine("Четверг");
+//     if(number_for_day_of_the_week == 5) Console.WriteLine("Пятница");
+//     if(number_for_day_of_the_week == 6) Console.WriteLine("Суббота");
+//     if(number_for_day_of_the_week == 7) Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Дня недели с таким номером не существует");
+// }
+
+// // Задача про два числа и является ли одно квадратом другого
